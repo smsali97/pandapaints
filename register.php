1f8165e618ca417540e691b04fcf12e67d34a554
Syed Mohammad Sualeh Ali <smsali97@gmail.com>
2018-09-25 00:00:04 +0500

    Changed the incorrect heading of register.php

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['id']))
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Register | iTech Empires: PHP State Management Tutorial</title>
+	<title>Register yourself on Panda Paints</title>
 
 	<!-- Bootstrap CSS File -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
