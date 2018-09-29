488d8582719fa9423ef1b7dd405bf86ffb642cf6
Syed Mohammad Sualeh Ali <smsali97@gmail.com>
2018-09-29 23:17:54 +0500

    added password

--- customers/ajax/db_connection.php
+++ customers/ajax/db_connection.php
@@ -2,7 +2,7 @@
 // Connection variables 
 $host = "localhost"; // MySQL host name eg. localhost
 $user = "root"; // MySQL user. eg. root ( if your on localserver)
-$password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
+$password = "sualeh"; // MySQL user password  (if password is not set for your root user then keep it empty )
 $database = "myshop_13005"; // MySQL Database name
  
 // Connect to MySQL Database
